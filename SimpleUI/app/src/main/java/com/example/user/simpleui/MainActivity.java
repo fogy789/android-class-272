a8f1d108e0111698b4895f6517554edaa620494e
fogy789 <103316104@gms.tcu.edu.tw>
2016-08-09 11:29:18 +0800

    add radiobutton

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,23 +3,46 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textview;
+    EditText ediText;
+    RadioGroup radioGroup;
 
+    String drink="Black Tea";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//指定使用activity_main這Layout的頁面
 
         textview = (TextView)findViewById(R.id.textView);
+        ediText =(EditText) findViewById(R.id.editText);
+        radioGroup=(RadioGroup)findViewById(R.id.rdg);
 
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.blackTea)
+                {
+                    drink="Black Tea";
+                }
+                else if(checkedId==R.id.greenTea)
+                {
+                    drink="Green Tea";
+                }
+            }
+        });
     }
 
     public void click(View view)
     {
-        textview.setText("Hello yangdang!!");
+        String text = ediText.getText().toString();
+        text = text + " Order" + drink;
+        textview.setText(text);
     }
 }
