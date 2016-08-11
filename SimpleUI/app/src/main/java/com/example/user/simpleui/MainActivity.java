df043cd3b431302d68ba8972d1916d44e22220a8
fogy789 <103316104@gms.tcu.edu.tw>
2016-08-11 10:01:07 +0800

    add LogMainActivity _Lifecycle

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -58,13 +59,15 @@ public class MainActivity extends AppCompatActivity {
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Order order = (Order)parent.getAdapter().getItem(position);//因為事物件，所以要轉型成Order
+                Order order = (Order) parent.getAdapter().getItem(position);//因為事物件，所以要轉型成Order
                 Toast.makeText(MainActivity.this, order.note, Toast.LENGTH_LONG).show();//若被包在listener內的話，'this會變成只向linstener，要MainActivity.this才會指向MainActivity
             }
         });
 
         setupListview();
         setupSpinner();
+
+        Log.d("DEBUG","MainActivity onCreate");
     }
 
     private void setupSpinner()
@@ -114,5 +117,41 @@ public class MainActivity extends AppCompatActivity {
         data.add(order);
         setupListview();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("DEBUG","MainActivity onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("DEBUG","MainActivity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("DEBUG", "MainActivity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("DEBUG", "MainActivity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("DEBUG", "MainActivity onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("DEBUG", "MainActivity onRestart");
     }
 }
