fa828159104005e803776555df35c5fa4354c26b
fogy789 <103316104@gms.tcu.edu.tw>
2016-08-18 12:12:56 +0800

    thread and handler

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.simpleui;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import android.os.Handler;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -15,7 +18,7 @@ public class OrderDetailActivity extends AppCompatActivity {
         TextView notetextview = (TextView)findViewById(R.id.notetextView);
         TextView storeInfotextview = (TextView)findViewById(R.id.storeinfotextView);
         TextView drinkOrderResultstextview = (TextView)findViewById(R.id.drinkorderresultstextView);
-        TextView latlngtextview = (TextView)findViewById(R.id.latlngtextView);
+        final TextView latlngtextview = (TextView)findViewById(R.id.latlngtextView);
 
         Intent intent = getIntent();
         Order order = intent.getParcelableExtra("order");
@@ -30,5 +33,27 @@ public class OrderDetailActivity extends AppCompatActivity {
             resultText += drinkName + " M:" + mNumber + "  L:"+lNumber + "\n";
         }
         drinkOrderResultstextview.setText(resultText);
+
+        final Handler handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                latlngtextview.setText("123, 456");
+                return false;
+            }
+        });
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    handler.sendMessage(new Message());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();//thread.run()會把工作丟給mainthread增加其工作量，途中有睡眠，間若太長榮義一片黑,thread.start()是background睡
     }
 }
